5b078550591bdc6364f5d689e7ff601449e66fab
Raisa01~ <zarvirova@mail.ru>
2022-11-25 19:10:29 +0300

    Task6.1

--- Program.cs
+++ Program.cs
@@ -37,9 +37,9 @@ int num = Convert.ToInt32(Console.ReadLine());
 
 if (num % 2 == 1)
 {
-    Console.WriteLine("Число" + num + "является нечетным"):
+    Console.WriteLine("Число" + num + "является нечетным");
 }
 else
 {
-    Console.WriteLine("Число" + num + "является четным"):
+    Console.WriteLine("Число" + num + "является четным");
 }
