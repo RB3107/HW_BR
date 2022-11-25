f83181a4f07152cc5c60489f6e1ae63f8d07e2cd
Raisa01~ <zarvirova@mail.ru>
2022-11-25 19:23:52 +0300

    Task8

--- Program.cs
+++ Program.cs
@@ -43,3 +43,24 @@ else
 {
     Console.WriteLine("Число" + num + "является четным");
 }*/
+
+//Задача 8 Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+int i = 1;
+bool not = true;
+Console.WriteLine ("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Четные числа от 1 до" + num );
+while (i<= num)
+{
+    if (i % 2 ! = 1)
+    {
+       Console.Write(i + ".");
+       not = false; 
+    }
+    i++;
+}
+if (not)
+{
+    Console.WriteLine("Нет Четных чисел!");
+}
